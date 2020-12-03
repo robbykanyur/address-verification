562bae643a41fdabf521dd1197eeafd4a90e7588
Robby Kanyur <robby@kanyur.com>
2020-12-03 01:17:02 -0700

    Retrieve record from airtable

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const Airtable = require('airtable');
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.json({
-    "text": "howdy"
+const app = express();
+const port = 5000;
+
+require('dotenv').config();
+
+const connect_to_airtable = () => {
+  const base = require('airtable').base('appy0AvTNsgCfdL7U');
+  return base;
+};
+
+const fetch_record_by_email = (email) => {
+  return new Promise((resolve, reject) => {
+    base('Addresses').select({filterByFormula: `{Borr Email} = "${email}"`})
+    .firstPage(function(err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function(r){
+        resolve(r.fields);
+      });
+    });
   });
+};
+
+const base = connect_to_airtable();
+
+app.get('/', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  let record = await fetch_record_by_email(req.query['email']);
+  console.log(record);
+  res.json({'status':200});
 });
 
 app.listen(port, () => {
